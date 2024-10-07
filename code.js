18d74ed6620c2c56c8844094a0bc1dff5a400f3a
Nile Young <nilevy03@gmail.com>
2024-10-06 18:06:10 -0600

    Update code.js

--- code.js
+++ code.js
@@ -6,3 +6,15 @@ function isSorted(arr) {
     }
     return true;
 }
+
+function permutationSort(arr) {
+  let shuffleCount = 0;
+  while (!isSorted(arr)) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    shuffleCount++;
+  }
+  return shuffleCount;
+}
