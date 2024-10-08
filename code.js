5340023415d7be6b174520a2716c0b7bc6204103
Nile Young <nilevy03@gmail.com>
2024-10-08 13:59:54 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,44 @@
 function isSorted(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i - 1] > arr[i]) {
-            return false;
-        }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] > arr[i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
+}
+
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function* generatePermutations(arr, start = 0) {
+  if (start === arr.length - 1) {
+    yield arr.slice();
+  } else {
+    for (let i = start; i < arr.length; i++) {
+      swap(arr, start, i);
+      yield* generatePermutations(arr, start + 1);
+      swap(arr, start, i);
+    }
+  }
 }
 
 function permutationSort(arr) {
+  if (arr.length <= 1) return 1;
+
   let shuffleCount = 0;
-  while (!isSorted(arr)) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+  for (const perm of generatePermutations(arr)) {
     shuffleCount++;
+    if (isSorted(perm)) {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = perm[i];
+      }
+      return shuffleCount;
+    }
   }
+
+
   return shuffleCount;
 }
